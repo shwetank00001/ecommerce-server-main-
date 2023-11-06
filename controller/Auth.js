1a8e3ff10f324a409f81deb0b272f3e23377633c
shwetank00001 <mshwetank@gmail.com>
2023-11-06 11:49:11 +0530

    added stability

--- controller/Auth.js
+++ controller/Auth.js
@@ -4,6 +4,7 @@ const { sanitizeUser, sendMail } = require('../services/common');
 const jwt = require('jsonwebtoken');
 
 exports.createUser = async (req, res) => {
+
   try {
     const salt = crypto.randomBytes(16);
     crypto.pbkdf2(
@@ -13,31 +14,55 @@ exports.createUser = async (req, res) => {
       32,
       'sha256',
       async function (err, hashedPassword) {
-        const user = new User({ ...req.body, password: hashedPassword, salt });
-        const doc = await user.save();
 
-        req.login(sanitizeUser(doc), (err) => {
-          // this also calls serializer and adds to session
-          if (err) {
-            res.status(400).json(err);
-          } else {
-            const token = jwt.sign(
-              sanitizeUser(doc),
-              process.env.JWT_SECRET_KEY
-            );
-            res
-              .cookie('jwt', token, {
-                expires: new Date(Date.now() + 3600000),
-                httpOnly: true,
-              })
-              .status(201)
-              .json({ id: doc.id, role: doc.role });
-          }
-        });
+
+        try {
+            const user = new User({
+                ...req.body,
+                password: hashedPassword,
+                salt,
+            });
+            const doc = await user.save();
+
+            req.login(sanitizeUser(doc), (err) => {
+                // this also calls serializer and adds to session
+                if (err) {
+                    res.status(400).json(err);
+                } else {
+                    const token = jwt.sign(
+                        sanitizeUser(doc),
+                        process.env.JWT_SECRET_KEY
+                    );
+                    res.cookie("jwt", token, {
+                        expires: new Date(Date.now() + 3600000),
+                        httpOnly: true,
+                    })
+                        .status(201)
+                        .json({ id: doc.id, role: doc.role });
+                }
+            });
+          
+        } catch (error) {
+          console.log("User Already exists !")
+          res.send({
+            success: false,
+            msg: error
+          })
+          
+        }
+
+
       }
     );
+
+    console.log("User added to DB Successfully !")
+
   } catch (err) {
-    res.status(400).json(err);
+    console.log(err)
+    res.json({
+      success: false,
+      msg: err
+    })
   }
 };
 
